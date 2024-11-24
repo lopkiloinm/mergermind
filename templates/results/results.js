5dabe6e9990085e256b724576a71bb8dab3bacf0
lopkiloinm <167727986+lopkiloinm@users.noreply.github.com>
2024-11-23 23:22:41 -0800

    mysql

--- templates/results/results.js
+++
@@ -1,5 +0,0 @@
-(function() {
-    const markdownContent = {{ result | tojson }};
-    const htmlContent = marked.parse(markdownContent);
-    document.getElementById("content-{{ loop.index }}").innerHTML = htmlContent;
-})();
